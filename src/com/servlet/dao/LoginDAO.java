a2b14c797b68891cb8432c0dd37c2f23c8e2ab7f
ldw1220 <dlehddnjs8297@gmail.com>
2022-03-09 17:30:48 +0900

    3.2 자바와 DB 간의 효율적인 자원 관리 가능하게 DB Connection pool 설정

--- src/com/servlet/dao/LoginDAO.java
+++ src/com/servlet/dao/LoginDAO.java
@@ -6,19 +6,21 @@ import java.sql.ResultSet;
 import java.sql.PreparedStatement;
 import java.util.ArrayList;
 
+import javax.naming.*;
+import javax.sql.*;
+
 import com.servlet.dto.LoginDTO;
 
 public class LoginDAO {
-    // driver loading을 위한 기본 정보
-    String database = "rmpdb"; // db이름
-    String driver = "com.mysql.cj.jdbc.Driver";
-    String url = "jdbc:mysql://localhost/" + database;
-    String id = "root";
-    String pw = "xldzmf12";
+	//커넥션 풀을 담기 위한 DataSource 타입의 변수를 선언
+	DataSource dataSource;
     
     public LoginDAO() {
     	try {
-    		Class.forName(driver);
+    		//Context를 생성
+    		Context context = new InitialContext();
+    		//context.xml에서 설정한 connection pool의 이름으로 dataSource에 전달
+    		dataSource = (DataSource)context.lookup("java:comp/env/jdbc/Mysql");
     	} catch (Exception e) {
     		e.printStackTrace();
     	}
@@ -33,7 +35,8 @@ public class LoginDAO {
 		ResultSet res = null;
     	
 		try {
-			con = DriverManager.getConnection(url, id, pw);
+			// dataSource로 부터 커넥션을 가져온다.
+			con = dataSource.getConnection();
 			String sql = "SELECT * FROM member";
 			pstmt = con.prepareStatement(sql);
 			res = pstmt.executeQuery(sql);
--- src/com/servlet/loginAction.java
+++ src/com/servlet/loginAction.java
@@ -48,6 +48,8 @@ public class loginAction extends HttpServlet {
 	}
 
 	
+	
+	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
 		doGet(request, response);
